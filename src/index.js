9eebaf37d39a233221a8ba14e2835a784c35a100
JLL-Rudenko-Dmitriy <jll.rudenkodmitriy@gmail.com>
2023-07-06 15:10:13 +0300

    add deploy command file

--- src/index.js
+++ src/index.js
@@ -1,16 +1,52 @@
+const fs = require('node:fs');
+const path = require('node:path');
+
 // Require the necessary discord.js classes
-const {Client, Events, GatewayIntentBits } = require('discord.js');
+const {Client, Collection, GatewayIntentBits } = require('discord.js');
 const {token} = require('../configuration/config.json');
-const logger = require('../utils/logfunctions.js')
+
 
 
 
 // Create an instance of the class Client
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-// Login Client instance in Discord
+
+client.commands = new Collection();
+
+const foldersPath = path.join(__dirname.replace('src',''), 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+        
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+            console.log(command);
+		} 
+        else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
+
+
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
 client.login(token);
-
-// Check bot status
-
-client.once(Events.ClientReady,c => logger.logClinetReady(c));
--- utils/logfunctions.js
+++ utils/logfunctions.js
@@ -4,11 +4,16 @@ const times = 40;
 
 const repeatS = (ch, timesR) => ch.repeat(timesR);
 
-const logClinetReady = (c) => 
-    console.log(
+const logClinetReady = (c) => console.log(
         repeatS('\\', times) + '\n'
         + `Everything is ok!\nLogged in as ${c.user.tag}. \nIs bot: ${c.user.bot}\n`
         + repeatS('\\',times));
 
+const logWarnCommandMiss = (fp) => console.log(
+        repeatS('|',times) + '\n' 
+        + `[WARNING] the command ${fp} is missing a required "data" or "execute" property`
+        + repeatS('|',times)); 
+
 module.exports.logClinetReady = logClinetReady;
+module.exports.logWarnCommandMiss = logWarnCommandMiss;
 module.exports.times = times;
