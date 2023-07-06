c7b26b0c2a3081deef5265c131ae09a5d5e51181
JLL-Rudenko-Dmitriy <jll.rudenkodmitriy@gmail.com>
2023-07-06 15:11:48 +0300

    split events

---
+++ src/events/ready.js
@@ -0,0 +1,10 @@
+const { Events } = require('discord.js');
+const logger  = require('../../utils/logfunctions.js')
+
+module.exports = {
+    name: Events.ClientReady,
+    once: true,
+    execute(client) {
+        logger.logClinetReady(client);
+    }
+};
