d12eae063eac6c8aba543e9d6253c6b54bfe7b44
JLL-Rudenko-Dmitriy <jll.rudenkodmitriy@gmail.com>
2023-07-06 15:12:20 +0300

    ping command

---
+++ commands/dev/ping.js
@@ -0,0 +1,12 @@
+const { SlashCommandBuilder, GatewayIntentBits } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('Replies with Pong [time]ms'),
+
+	async execute(interaction) {
+		const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true });
+		interaction.editReply(`Roundtrip latency: ${sent.createdTimestamp - interaction.createdTimestamp}ms`);
+	},
+}
